e135e76a4864a4787ed2d4c88f9df7b76a14fcc4
simpleframework <cknet@126.com>
2013-11-15 12:09:49 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/app/template/SFTemplateT2.java
+++ src/main/java/net/simpleframework/app/template/SFTemplateT2.java
@@ -15,8 +15,7 @@ import net.simpleframework.mvc.template.t2.AbstractTemplateHandlerT2;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class SFTemplateT2 extends AbstractTemplateHandlerT2 implements ISFTemplateConst {
