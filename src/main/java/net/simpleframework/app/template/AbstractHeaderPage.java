56e91ccd470ad120c19ae9ecea56caf76ac4a7f8
simpleframework <cknet@126.com>
2013-11-15 11:31:03 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/app/template/AbstractHeaderPage.java
+++ src/main/java/net/simpleframework/app/template/AbstractHeaderPage.java
@@ -11,7 +11,7 @@ import net.simpleframework.organization.web.page.attri.AbstractEditAwarePage;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public abstract class AbstractHeaderPage extends AbstractEditAwarePage {
--- src/main/java/net/simpleframework/app/template/SFTemplateT2.java
+++ src/main/java/net/simpleframework/app/template/SFTemplateT2.java
@@ -16,7 +16,7 @@ import net.simpleframework.mvc.template.t2.AbstractTemplateHandlerT2;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class SFTemplateT2 extends AbstractTemplateHandlerT2 implements ISFTemplateConst {
